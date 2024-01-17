b9a3e75c0d6fe47defa2917aff6cb108229716bc
AdamKiss <21.kiss.adam@gmail.com>
2024-01-17 22:28:25 +0100

    404 error page

--- src/app/components/error-pages/invalid-route/invalid-route.component.ts
+++ src/app/components/error-pages/invalid-route/invalid-route.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-invalid-route',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './invalid-route.component.html',
-  styleUrl: './invalid-route.component.scss'
+  styleUrl: './invalid-route.component.scss',
 })
-export class InvalidRouteComponent {
+export class InvalidRouteComponent implements OnInit {
+  countdown: number;
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.countdown = 10;
+    this.countdownTimer();
+  }
+
+  countdownTimer() {
+    if (this.countdown > 0) {
+      setTimeout(() => {
+        this.countdown = this.countdown - 1;
+        this.countdownTimer();
+      }, 1000);
+    } else {
+      this.router.navigate(['']);
+      return;
+    }
+  }
 }
